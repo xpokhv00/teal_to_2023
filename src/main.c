ea88c63a2047cbdcad8f370a97d01b2c8a360967
Ondřej Mach <ondrej.mach@seznam.cz>
2021-12-05 19:33:06 +0100

    Minor refactoring to make main look better.

--- src/main.c
+++ src/main.c
@@ -11,6 +11,20 @@
 
 #include "main.h"
 
+static const char *errorStrings[] = {
+        [SUCCESS] = "", // never happens
+        [ERR_LEXICAL] = "Lexical",
+        [ERR_SYNTAX] = "Syntax",
+        [ERR_SEMANTIC_DEF] = "Semantic - undefined variable / function, or attempted function / variable redefinition (DEF)",
+        [ERR_SEMANTIC_ASSIGN] = "Semantic - type incompatibility (ASSIGN)",
+        [ERR_SEMANTIC_FUNC] = "Semantic - wrong number / type of parameters / returns in function call (FUNC)",
+        [ERR_SEMANTIC_EXPR] = "Semantic - type incompatibility in arithmetic / string / relation expressions (EXPR)",
+        [ERR_SEMANTIC_OTHER] = "Semantic - other (OTHER)",
+        [ERR_RUNTIME_NIL] = "Runtime",
+        [ERR_RUNTIME_DIV_ZERO] = "Runtime",
+        [ERR_INTERNAL] = "Internal",
+};
+
 int main() {
     scanner_init(stdin);
 
@@ -18,19 +32,6 @@ int main() {
     if (s == SUCCESS) {
         fprintf(stderr, "Compilation was successful!\n");
     } else {
-        char *errorStrings[] = {
-                [SUCCESS] = "", // never happens
-                [ERR_LEXICAL] = "Lexical",
-                [ERR_SYNTAX] = "Syntax",
-                [ERR_SEMANTIC_DEF] = "Semantic - undefined variable / function, or attempted function / variable redefinition (DEF)",
-                [ERR_SEMANTIC_ASSIGN] = "Semantic - type incompatibility (ASSIGN)",
-                [ERR_SEMANTIC_FUNC] = "Semantic - wrong number / type of parameters / returns in function call (FUNC)",
-                [ERR_SEMANTIC_EXPR] = "Semantic - type incompatibility in arithmetic / string / relation expressions (EXPR)",
-                [ERR_SEMANTIC_OTHER] = "Semantic - other (OTHER)",
-                [ERR_RUNTIME_NIL] = "Runtime",
-                [ERR_RUNTIME_DIV_ZERO] = "Runtime",
-                [ERR_INTERNAL] = "Internal",
-        };
         fprintf(stderr, "Compilation failed with %s error\n", errorStrings[s]);
     }
     scanner_destroy();
